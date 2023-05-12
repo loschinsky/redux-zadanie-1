ccc1c3a94c3a0c865ebaf90f9e9e48f8974e0e29
loschinsky <tank120398@gmail.com>
2023-05-12 11:04:40 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import { taskCompleted, taskDeleted, titleChanged } from './store/actions.js'
-
-import { initiateStore } from './store/store'
+import { taskCompleted, taskDeleted, titleChanged } from './store/task'
+import configureStore from './store/store'
 const initialState = [
   { id: 1, title: 'Task 1', completed: false },
   { id: 2, title: 'Task 2', completed: false },
 ]
-const store = initiateStore()
+const store = configureStore()
 const App = (params) => {
   const [state, setState] = useState(store.getState())
   useEffect(() => {
@@ -35,7 +34,7 @@ const App = (params) => {
             <p>{`Completed:${el.completed}`}</p>
             <button onClick={() => completeTask(el.id)}>Complete</button>
             <button onClick={() => changeTitle(el.id)}>Change tittle</button>
-            <button onClick={()=> deleteTask(el.id)}>Delete</button>
+            <button onClick={() => deleteTask(el.id)}>Delete</button>
             <hr />
           </li>
         ))}
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,7 @@
 import { legacy_createStore as createStore } from 'redux'
-import { taskReducer } from './taskReducer'
-const initialState = [
-  { id: 1, title: 'Task 1', completed: false },
-  { id: 2, title: 'Task 2', completed: false },
-]
+import taskReducer from './task'
 
-export function initiateStore() {
-  return createStore(taskReducer, initialState)
+function configureStore() {
+  return createStore(taskReducer)
 }
+export default configureStore
